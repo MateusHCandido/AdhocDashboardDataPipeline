83226d2ce5f4151261dcac820331bbf579d441af
Mateuzo <mateushcandido5@gmail.com>
2024-12-03 11:51:41 -0300

    importing copy files

---
+++ sql_scripts/copy/copy_produtos.sql
@@ -0,0 +1,7 @@
+COPY produtos
+FROM 'sua_uri_produtos'
+CREDENTIALS 'aws_access_key_id=sua_key_id;aws_secret_access_key=seu_secret_access'
+REGION 'us-east-1' --região em que o bucket foi criado
+DELIMITER ';' 
+IGNOREHEADER 1 -- ignora cabecalho
+DATEFORMAT 'DD/MM/YYYY';
